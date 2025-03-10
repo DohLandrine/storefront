fd16e01c8edaa562cd251f67862f896fa4e50652
Doh Landrine <landrinedoh22@gmail.com>
2025-03-09 21:51:43 -0700

    Created one to many relationships between classes.

--- playground/models.py
+++ playground/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+class Collection(models.Model):
+    title = models.CharField(max_length = 255),
+
 class Product(models.Model):
     title = models.CharField(max_length = 255),
     description = models.TextField(),
@@ -7,7 +10,8 @@ class Product(models.Model):
     # From below, a number with 6 digits and  2 decimal place is 9999.99
     price = models.DecimalField(max_digits = 6, decimal_places = 2)
     inventory = models.IntegerField(),
-    last_update = models.DateTimeField(auto_now = True)
+    last_update = models.DateTimeField(auto_now = True),
+    collection = models.ForeignKey(Collection, on_delete = models.PROTECT)
 
 class Customer(models.Model):
     first_name = models.CharField(max_length = 255),
@@ -38,9 +42,24 @@ class Order(models.Model):
         (payment_status_failed, 'failed'),
         (payment_status_complete, 'complete')
     ]
-    payment_status = models.CharField(max_length = 1, choices = PAYMENT_STATUS_CHOICES, default = payment_status_pending)
+    payment_status = models.CharField(max_length = 1, choices = PAYMENT_STATUS_CHOICES, default = payment_status_pending),
+    customer = models.ForeignKey(Customer, on_delete = models.PROTECT)
 
 class Address(models.Model):
     city = models.CharField(max_length = 255),
     street = models.CharField(max_length = 255),
     customer = models.OneToOneField(Customer, on_delete = models.CASCADE, primary_key = True)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add = True),
+
+class OrderItem(models.Model):
+    quantity = models.PositiveSmallIntegerField(),
+    unit_price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    order = models.ForeignKey(Order, on_delete = models.PROTECT)
+    product = models.ForeignKey(Product, on_delete = models.PROTECT)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete = models.CASCADE),
+    product = models.ForeignKey(Product, on_delete = models.CASCADE),
+    quantity = models.PositiveSmallIntegerField()
